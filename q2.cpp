dba0b9cea8e762ba3280301327f75a761a05cc4d
Derick Jin <jinderick5@gmail.com>
2022-10-10 13:13:18 -0700

    main

--- q2.cpp
+++ q2.cpp
@@ -25,7 +25,13 @@ int findMin(int& n1, int&n2, int&n3)
 	return min;
 }
 
-
+int main()
+{
+	int n1, n2, n3, min;
+	getInput(n1, n2, n3);
+	min = findMin(n1, n2, n3);
+	printResult(n1, n2, n3, min);
+}
 
 
 // ******************************
