48df81c5457ed4492573dd08cf40690462c82140
Derick Jin <jinderick5@gmail.com>
2022-10-10 13:13:03 -0700

    findmin

--- q2.cpp
+++ q2.cpp
@@ -9,6 +9,23 @@ void getInput(int& n1, int& n2, int& n3)
 	cout << "Enter three numbers: ";
 	cin >> n1 >> n2 >> n3;
 }
+int findMin(int& n1, int&n2, int&n3)
+{
+	int min = n1;
+	if (min > n2)
+	{
+		min = n2;
+	}
+
+	if (min > n3)
+	{
+		min = n3;
+	}
+	
+	return min;
+}
+
+
 
 
 // ******************************
