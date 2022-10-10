8090798ce21ba55b6c9e819309705747e8204e48
Derick Jin <jinderick5@gmail.com>
2022-10-10 13:12:48 -0700

    getInput

--- q2.cpp
+++ q2.cpp
@@ -10,29 +10,6 @@ void getInput(int& n1, int& n2, int& n3)
 	cin >> n1 >> n2 >> n3;
 }
 
-int findMin(int& n1, int&n2, int&n3)
-{
-	int min = n1;
-	if (min > n2)
-	{
-		min = n2;
-	}
-
-	if (min > n3)
-	{
-		min = n3;
-	}
-	
-	return min;
-}
-
-int main()
-{
-	int n1, n2, n3, min;
-	getInput(n1, n2, n3);
-	min = findMin(n1, n2, n3);
-	printResult(n1, n2, n3, min);
-}
 
 // ******************************
 // this function will help you pass the test. When you print the result with this function, you will get the same output format
