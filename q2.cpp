c221ab318b4671ae630b1d4bb9da9f0f6bad204b
Derick Jin <jinderick5@gmail.com>
2022-10-10 13:11:31 -0700

    1

--- q2.cpp
+++ q2.cpp
@@ -2,9 +2,37 @@
 #include <iomanip>
 using namespace std;
 
+void printResult(int n1, int n2, int n3, int min);
 // ******************************
-// Your code here
-// ******************************
+void getInput(int& n1, int& n2, int& n3)
+{
+	cout << "Enter three numbers: ";
+	cin >> n1 >> n2 >> n3;
+}
+
+int findMin(int& n1, int&n2, int&n3)
+{
+	int min = n1;
+	if (min > n2)
+	{
+		min = n2;
+	}
+
+	if (min > n3)
+	{
+		min = n3;
+	}
+	
+	return min;
+}
+
+int main()
+{
+	int n1, n2, n3, min;
+	getInput(n1, n2, n3);
+	min = findMin(n1, n2, n3);
+	printResult(n1, n2, n3, min);
+}
 
 // ******************************
 // this function will help you pass the test. When you print the result with this function, you will get the same output format
